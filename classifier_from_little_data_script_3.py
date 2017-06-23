b77a3b6a2251fc0d15e9c1303e3b9d582e2f8639
JaminZhou <zhoujiamin1992@163.com>
2017-06-23 18:26:06 +0800

    fix ValueError: The shape of the input to "Flatten" is not fully defined (got (None, None, 512). Make sure to pass a complete "input_shape" or "batch_input_shape" argument to the first layer in your model.

--- classifier_from_little_data_script_3.py
+++ classifier_from_little_data_script_3.py
@@ -39,29 +39,31 @@ data/
 from keras import applications
 from keras.preprocessing.image import ImageDataGenerator
 from keras import optimizers
-from keras.models import Sequential
-from keras.layers import Dropout, Flatten, Dense
+from keras.models import Sequential, Model
+from keras.layers import Dropout, Flatten, Dense, Input
 
 # path to the model weights files.
-weights_path = '../keras/examples/vgg16_weights.h5'
-top_model_weights_path = 'fc_model.h5'
+#weights_path = '../keras/examples/vgg16_weights.h5'
+top_model_weights_path = 'bottleneck_fc_model.h5'
+full_model_weights_path = 'full_model.h5'
 # dimensions of our images.
 img_width, img_height = 150, 150
 
-train_data_dir = 'cats_and_dogs_small/train'
-validation_data_dir = 'cats_and_dogs_small/validation'
+train_data_dir = 'data/train'
+validation_data_dir = 'data/validation'
 nb_train_samples = 2000
 nb_validation_samples = 800
 epochs = 50
 batch_size = 16
 
 # build the VGG16 network
-model = applications.VGG16(weights='imagenet', include_top=False)
+input_tensor = Input(shape=(img_height, img_width, 3))
+base_model = applications.VGG16(weights='imagenet', include_top=False, input_tensor=input_tensor)
 print('Model loaded.')
 
 # build a classifier model to put on top of the convolutional model
 top_model = Sequential()
-top_model.add(Flatten(input_shape=model.output_shape[1:]))
+top_model.add(Flatten(input_shape=base_model.output_shape[1:]))
 top_model.add(Dense(256, activation='relu'))
 top_model.add(Dropout(0.5))
 top_model.add(Dense(1, activation='sigmoid'))
@@ -72,7 +74,8 @@ top_model.add(Dense(1, activation='sigmoid'))
 top_model.load_weights(top_model_weights_path)
 
 # add the model on top of the convolutional base
-model.add(top_model)
+#model.add(top_model)
+model = Model(input= base_model.input, output= top_model(base_model.output))
 
 # set the first 25 layers (up to the last conv block)
 # to non-trainable (weights will not be updated)
@@ -113,3 +116,8 @@ model.fit_generator(
     epochs=epochs,
     validation_data=validation_generator,
     nb_val_samples=nb_validation_samples)
+
+model.save_weights(full_model_weights_path)
+
+from keras.utils import plot_model
+plot_model(model, to_file='model.png')
