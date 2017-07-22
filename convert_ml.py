49f0e1431f8c838dba25fbffcbe183dd52234a71
JaminZhou <zhoujiamin1992@163.com>
2017-07-22 22:52:45 +0800

    update convert_ml

--- convert_ml.py
+++ convert_ml.py
@@ -2,5 +2,15 @@ from keras.models import load_model
 import coremltools
 
 model = load_model('model')
-coreml_model = coremltools.converters.keras.convert(model)
+coreml_model = coremltools.converters.keras.convert(model, input_names = 'image', image_input_names = 'image', output_names = 'output')
+coreml_model.author = 'JaminZhou'
+coreml_model.license = 'MIT'
+coreml_model.short_description = 'Cat or Dog'
+coreml_model.input_description['image'] = 'image: CVPixelBuffer'
+coreml_model.output_description['output'] = 'output: 0 or 1'
 coreml_model.save("keras_model.mlmodel")
+
+#let keras = keras_model()
+#let imageBuffer: CVPixelBuffer
+#let prediction = try keras1.prediction(image: imageBuffer)
+#prediction.output[0] //the value 0 is cat, value 1 is dog
